98260e87d768898b693b9afe5959cd9dc7897abd
User <klyev.alex.02@gmail.com>
2022-09-22 21:15:28 +0700

    added pointer to function

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -11,38 +11,21 @@ double FunB(double x) {
     return  2.7 * std::pow(std::sin(x), 3) + 3.3 * std::pow(std::cos(x), 3);
 }
 
-void Print(std::string name,double esp, double point, int counter){
-
+void Print(std::string name, double(*Fun)(double), double esp, double point, int counter){
+    std::string fun = Fun == FunA ? "FunA" : "FunB";
     std::cout << "Method " << name << std::endl;
-    std::cout << "Eps = " << esp << "\nPoint min = " << point << "\nFunction value to min point "<< FunA(point) << "\nCounter = " << counter << std::endl;
+    std::cout << fun << std::endl;
+    std::cout << "Eps = " << esp << "\nPoint min = " << point << "\nFunction value to min point "<< Fun(point) << "\nCounter = " << counter << std::endl;
 }
 
-void Dichotomy(double esp, double a,double b, double delta = 0.001){
-    int counter = 0; double x1,x2;
+void Dichotomy(double(*Fun)(double), double esp, double a, double b, double delta = 0.001) {
+    int counter = 0; double x1, x2;
 
     while ((b - a) / 2 > esp) {
         x1 = (a + b - delta) / 2;
         x2 = (a + b + delta) / 2;
 
-        if (FunA(x1) <= FunA(x2)) 
-            b = x2;
-        else 
-            a = x1;
-        
-        counter++;
-    }
-
-    Print("Dichotomy", esp, (a + b) / 2, counter);
-}
-
-void GoldenSection(double esp, double a, double b) {
-    int counter = 0; double x1, x2;
-
-    while ((b - a) / 2 > esp) {
-        x1 = a + (3 - std::sqrt(5)) * (b - a) / 2;
-        x2 = a + (std::sqrt(5) - 1) * (b - a) / 2;
-
-        if (FunA(x1) <= FunA(x2))
+        if (Fun(x1) <= Fun(x2))
             b = x2;
         else
             a = x1;
@@ -50,7 +33,25 @@ void GoldenSection(double esp, double a, double b) {
         counter++;
     }
 
-    Print("GoldenSection", esp, (a + b) / 2, counter);
+    Print("Dichotomy", Fun, esp, (a + b) / 2, counter);
+}
+
+void GoldenSection(double(*Fun)(double),double esp, double a, double b) {
+    int counter = 0; double x1, x2;
+
+    while ((b - a) / 2 > esp) {
+        x1 = a + (3 - std::sqrt(5)) * (b - a) / 2;
+        x2 = a + (std::sqrt(5) - 1) * (b - a) / 2;
+
+        if (Fun(x1) <= Fun(x2))
+            b = x2;
+        else
+            a = x1;
+
+        counter++;
+    }
+
+    Print("GoldenSection", Fun, esp, (a + b) / 2, counter);
 }
 
 int main()
@@ -60,14 +61,15 @@ int main()
     std::cin >> a >> b;
     double esp1 = 0.001;
     double esp2 = 0.0001;
+    double(*Fun)(double) = FunA;
 
-    Dichotomy(esp1, a, b, esp1);
+    Dichotomy(Fun,esp1, a, b, esp1);
     std::cout << "\n\n";
-    Dichotomy(esp2, a, b, esp2);
+    Dichotomy(Fun, esp2, a, b, esp2);
     std::cout << "\n\n";
-    GoldenSection(esp1, a, b);
+    GoldenSection(Fun, esp1, a, b);
     std::cout << "\n\n";
-    GoldenSection(esp2, a, b);
+    GoldenSection(Fun, esp2, a, b);
     
     return 0;
 }
